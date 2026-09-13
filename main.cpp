ce007fa5c4a946f90d4dbd9dbb114da6f84a5ed2
hahaz <Gleb.meleshko.99@mail.ru>
2026-09-13 22:40:47 +0700

    Добавьте файлы проекта.

---
+++ main.cpp
@@ -0,0 +1,77 @@
+#include <stdio.h>
+#include <math.h>  
+
+//Задание 1
+//int main()
+//{
+//    float y, t, z, x;
+//    y = 2;
+//    t = 5 / (1 + y * y);
+//    z = 4;
+//    x = 2 * y + 3 * sinh(t)-z;
+//    printf("%f", x);
+//    return 0;
+//}
+
+//Задание 2
+//int main()
+//{
+//	float y, t, z, x;
+//	t = 0.5;
+//	z = 6;
+//	y = t + 2 * cos(z) / sin(z);
+//	x = 3 * y * y / (4 * tan(z) - 2 * t * t);
+//	printf("%f", x);
+//	return 0;
+//}
+
+//Задание 3
+//int main()
+//{
+//	float x, t, z, y;
+//	t = 1;
+//	z = 3;
+//	y = sin(t);
+//	x = 4 * y * y / (4 * y * exp(z) - 2 * t * t * t);
+//	printf("%f", x);
+//	return 0;
+//}
+
+//Задание 4
+//int main()
+//{
+//	float x, t, z, y;
+//	t = 2;
+//	z = 3;
+//	y = cos(t + z);
+//	x = 4 * log(y * y * y) - z/t;
+//	printf("%f", x);
+//	return 0;
+//
+//}
+
+//Задание 5
+//int main()
+//{
+//	float x, t, z, y;
+//	y = 2;
+//	z = 4;
+//	t = sin(2 + z);
+//	x = 6 * t * t - (cos(z)/sin(z) + 1) / (y * y);
+//	printf("%f", x);
+//	return 0;
+//
+//}
+
+//Задание 6
+//int main()
+//{
+//	float x, z, t, y;
+//	z = 1;
+//	t = 2;
+//	y = tan(t)+z;
+//	x = (8*(z*z)+1)/(y*exp(t)+(t*t));
+//	printf("%f", x);
+//	return 0;
+//
+//}
